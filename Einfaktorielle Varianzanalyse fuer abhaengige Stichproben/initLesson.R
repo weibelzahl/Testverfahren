5ea7b340779d03bcdd98cc45a5c1ee4d6718c852
weibelzahl <pfh>
2016-07-01 22:08:52 +0200

    streamline variables and sentences

--- Einfaktorielle Varianzanalyse fuer abhaengige Stichproben/initLesson.R
+++ Einfaktorielle Varianzanalyse fuer abhaengige Stichproben/initLesson.R
@@ -1,6 +1,8 @@
 # remove motivational message
 options(swirl_is_fun = FALSE)
 options(swirl_language = "german")
+# options(swirl_is_fun = TRUE)
+# options(swirl_language = "german_formal")
 
 # Put initialization code in this file. The variables you create
 # here will show up in the user's workspace when he or she begins
--- Einfaktorielle Varianzanalyse fuer unabhaengige Stichproben/initLesson.R
+++ Einfaktorielle Varianzanalyse fuer unabhaengige Stichproben/initLesson.R
@@ -2,12 +2,8 @@
 options(swirl_is_fun = FALSE)
 options(swirl_language = "german")
 
-# Put initialization code in this file. The variables you create
-# here will show up in the user's workspace when he or she begins
-# the lesson.
-
-Daten <- data.frame(Gruppe = factor(x=c(replicate(10,1), replicate(10,2), replicate(10,3)),levels=c("1","2","3"), 
-                                    labels = c("Stille","Lärm","Musik")), 
+daten <- data.frame(Gruppe = factor(x=c(replicate(10,1), replicate(10,2), replicate(10,3)),levels=c("1","2","3"), 
+                                    labels = c("Stille","Laerm","Musik")), 
                     Erinnerung = c(c(70,57,59,60,51,30,52,62,59,57),c(41,26,28,47,47,39,50,36,48,25),c(37,47,46,55,45,57,51,60,60,51)))
 
                     # 1: Stille,  round(rnorm(10,52,10),0)
